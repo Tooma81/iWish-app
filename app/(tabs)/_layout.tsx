aa284306929cd37b1d11d28d3e6cd493b48de8f2
Toomas Kaarel <toomas.kaarel@gmail.com>
2025-11-23 02:00:38 +0200

    Started using Feather icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,14 +20,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.circle" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="friends"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Friends',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="heart.circle" color={color} />,
         }}
       />
     </Tabs>
